115d780e677bdafc45f12195b9f42eb1bc53b540
CristianPinto56 <cristianantoniio787@gmail.com>
2026-06-21 23:38:48 -0400

    Restricciones a seguir de inputs

--- gestor-biblioteca/src/GestorPrestamos.jsx
+++ gestor-biblioteca/src/GestorPrestamos.jsx
@@ -13,7 +13,7 @@ function GestorPrestamos(){
         },[prestamos])
 
     const nombreRef=useRef();
-    const rutRef=useRef();
+    const rutRef=useRef(); {/* Para el rut para ocupar una expresion regular, en el input type del rut luego del text poner esto: pattern="[0-9]{7,8}-[0-9kK]*/}
     const libroRef=useRef();
     const fechaRef=useRef();
 
@@ -29,11 +29,11 @@ function GestorPrestamos(){
             <form>
                 <div class="form-group">
                     <label for="InputNombre"> Nombre</label>
-                    <input ref={nombreRef} placeholder="Nombre" className="form-control form-control-lg" type="text" name="" id="" ></input>
+                    <input ref={nombreRef} placeholder="Nombre" className="form-control form-control-lg" type="text" minLength={"4"} maxLength={"40"} required name="" id="" ></input>
                 </div>
                 <div class="form-group">
                     <label for="InputLibro"> Libro</label>
-                    <input ref={libroRef} placeholder="Libro" className="form-control form-control-lg" type="text" name="" id="" ></input>
+                    <input ref={libroRef} placeholder="Libro" className="form-control form-control-lg" type="text"  minLength={"4"} maxLength={"40"} required name="" id="" ></input>
                 </div>
                 <button type="submit" class="btn btn-primary">Ingresar</button>
             </form>    
